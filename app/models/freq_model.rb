86f614cd4a62024fd863673521ed3e06fb634799
Eric Rochester <err8n@virginia.edu>
2011-11-15 10:47:14 -0500

    Added tests and freq method.

--- app/models/freq_model.rb
+++ app/models/freq_model.rb
@@ -10,4 +10,12 @@ class FreqModel
   def normalize(token)
     token.gsub(/\W/, '').downcase
   end
+
+  def freqs
+    counts = Hash.new(0)
+    tokens.each do |token|
+      counts[token] += 1
+    end
+    counts
+  end
 end
--- spec/models/freq_model_spec.rb
+++ spec/models/freq_model_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FreqModel do
-  describe "tokenizer" do
+  describe "tokens" do
     before(:each) do
       fm = FreqModel.new('The quick brown fox jumped over the lazy dog.')
       @tokens = fm.tokens
@@ -21,6 +21,27 @@ describe FreqModel do
       @tokens.each do |token|
         (/[A-Z]/ =~ token).should == nil
       end
+    end
+  end
+
+  describe "freqs" do
+    before(:each) do
+      fm = FreqModel.new('The quick brown fox jumped over the lazy dog.')
+      @freqs = fm.freqs
+    end
+
+    it 'should correctly count the tokens.' do
+      freqs = @freqs.to_a.sort
+      freqs.should == [
+        ["brown", 1],
+        ["dog", 1],
+        ["fox", 1],
+        ["jumped", 1],
+        ["lazy", 1],
+        ["over", 1],
+        ["quick", 1],
+        ["the", 2],
+      ]
     end
   end
 end
